a507de909041507e9724ab71bb5a8c48c5024049
idobalul <89575272+idobalul@users.noreply.github.com>
2022-02-07 13:58:36 +0200

    minor fixes
    changed some variables names to be more descriptive

--- controllers/main.go
+++ controllers/main.go
@@ -17,30 +17,30 @@ var pastes []models.Paste
 // It scrapes the pastebin and stores the pastes in the database.
 func Scrape() {
 	// Initiate the collector
-	c := colly.NewCollector()
+	collector := colly.NewCollector()
 
 	// Use proxy to connect to TOR
 	rp, err := proxy.RoundRobinProxySwitcher("socks5://127.0.0.1:9050")
 	if err != nil {
 		log.Fatal(err)
 	}
-	c.SetProxyFunc(rp)
+	collector.SetProxyFunc(rp)
 
 	// for every paste block on the page collect the title, content, author and date
-	c.OnHTML(".col-sm-12", func(h *colly.HTMLElement) {
+	collector.OnHTML(".col-sm-12", func(container *colly.HTMLElement) {
 		var content []string
 
 		// get the title
-		title := strings.Trim(h.ChildText("h4"), " \n")
+		title := strings.Trim(container.ChildText("h4"), " \n")
 
 		// get the content
-		h.ForEach("li", func(i int, e *colly.HTMLElement) {
-			content = append(content, strings.Trim(e.Text, " \n"))
+		container.ForEach("li", func(i int, element *colly.HTMLElement) {
+			content = append(content, strings.Trim(element.Text, " \n"))
 		})
 
 		// Only if the paste has a tittle continue normally
 		if title != "" {
-			rawInfo := strings.Trim(h.ChildText(".pre-footer"), " \n")
+			rawInfo := strings.Trim(container.ChildText(".pre-footer"), " \n")
 			info := strings.Split(rawInfo, "by")
 			authorAndDate := strings.Split(info[1], "at")
 
@@ -58,7 +58,7 @@ func Scrape() {
 		}
 	})
 
-	c.Visit("http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all")
-	c.Wait()
+	collector.Visit("http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all")
+	collector.Wait()
 	db.AddPastes(pastes)
 }
